ed485693b2e12f9c62be066b3aa2f73ffc3fbade
White Fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2020-06-07 21:30:29 +0300

    Started to experiment with Avalonia UI

---
+++ Client/FloaderClientGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FloaderClientGUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+
+        public string Test => "Yiffy";
+    }
+}
