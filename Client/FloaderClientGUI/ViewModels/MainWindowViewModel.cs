dfbb7a59681a26ad551c454a4fbac7527bdf8f7c
White Fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2020-07-05 17:44:05 +0300

    Clearing console / copying to clipboard (hangs)

--- Client/FloaderClientGUI/ViewModels/MainWindowViewModel.cs
+++ Client/FloaderClientGUI/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
+using TextCopy;
 
 namespace FloaderClientGUI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        /// <Summary>
+        /// Console log
+        /// </Summary>
+        private string _consoleText;
 
-        public string Test => "Yiffy";
+        public string ConsoleText
+        {
+            get => _consoleText;
+            set => this.RaiseAndSetIfChanged(ref _consoleText, value);
+        }
+
+        public MainWindowViewModel() : base()
+        {
+            ConsoleText = "Yerf!";
+        }
+
+        /// <summary>
+        /// Command to clear console
+        /// </summary>
+        public void ClearConsole()
+        {
+            ConsoleText = string.Empty;
+        }
+
+        /// <summary>
+        /// Copies console content to clipboard
+        /// </summary>
+        public void CopyConsoleToClipboard()
+        {
+            ClipboardService.SetText(ConsoleText);
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Almost ten years ago I've wrote opensource (GPLv3) bootloader for AVR MCUs (in assmebler) and client software for it (in C++/Qt). I've abandoned that project, but found, that is still being used at least by one person.
 
 So, I'm going to revive it, refactor assembler part, and rewrite client in .NET Core.
+
+Notes for myself (later will format it):
+Under *nix xclip have to be installed.
+
+Used 3rd party code:
+https://github.com/CopyText/TextCopy (MIT)
