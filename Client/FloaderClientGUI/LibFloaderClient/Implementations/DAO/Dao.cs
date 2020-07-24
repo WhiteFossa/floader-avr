0a07c578570dae96a6f5ca04b16ba23ae4bbfd64
White Fossa <fossa@fossa-main.yerrkhao-highspeed.whitefossa.org>
2020-07-24 22:28:33 +0300

    Able to read data from DB

--- Client/FloaderClientGUI/LibFloaderClient/Implementations/DAO/Dao.cs
+++ Client/FloaderClientGUI/LibFloaderClient/Implementations/DAO/Dao.cs
@@ -1,12 +1,42 @@
+using System;
+using System.Data.SQLite;
+using Dapper;
 using LibFloaderClient.Interfaces.DAO;
 
 namespace LibFloaderClient.Implementations.DAO
 {
     public class Dao : IDao
     {
+        /// <summary>
+        /// Path from executable directory to devices database
+        /// </summary>
+        private const string DbFilename = @"/DevDB.sqlite";
+
         public string GetVendorName(int vendorId)
         {
-            return "Fossa";
+            using (var connection = GetDbConnection())
+            {
+                connection.Open();
+
+                return connection
+                    .QueryFirst<string>($"select Name from vendors_names where Id={ vendorId }");
+            }
+        }
+
+        /// <summary>
+        /// Returns full path to device DB
+        /// </summary>
+        private string GetFullDbPath()
+        {
+            return $"{ Environment.CurrentDirectory }{ DbFilename }";
+        }
+
+        /// <summary>
+        /// Returns ready to use device DB connection
+        /// </summary>
+        private SQLiteConnection GetDbConnection()
+        {
+            return new SQLiteConnection($"Data Source={ GetFullDbPath() }");
         }
     }
 }
