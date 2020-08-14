5df0155d311aef40d503ee055a2c41eda1b99d1f
White Fossa <whitefossa@gmail.com>
2020-08-14 06:51:15 +0300

    Base record to bytes list done, tests are written and green.

---
+++ Client/FloaderClientGUI/LibIntelHex/Models/RecordBase.cs
@@ -0,0 +1,83 @@
+﻿using LibIntelHex.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibIntelHex.Models
+{
+    /// <summary>
+    /// Base class for all types of Intel HEX records
+    /// </summary>
+    public class RecordBase
+    {
+        /// <summary>
+        /// Maximal possible data length
+        /// </summary>
+        private const int MaxDataLength = 255;
+
+        /// <summary>
+        /// Maximal possible address
+        /// </summary>
+        private const int MaxAddress = 0xFFFF;
+
+        /// <summary>
+        /// Data field length
+        /// </summary>
+        public byte DataLength { get; }
+
+        /// <summary>
+        /// Address / offset
+        /// </summary>
+        public int Address { get; }
+
+        /// <summary>
+        /// Record type
+        /// </summary>
+        public RecordType Type { get; }
+
+        /// <summary>
+        /// Record payload
+        /// </summary>
+        public List<byte> Data { get; }
+
+        public RecordBase(int address, RecordType type, List<byte> data)
+        {
+            if (address < 0 || address > MaxAddress)
+            {
+                throw new ArgumentOutOfRangeException(nameof(address), address, $"Address must be within [0, { MaxAddress }] interval.");
+            }
+
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (data.Count > MaxDataLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(data.Count), data.Count, $"Data must be no longer than { MaxDataLength }.");
+            }
+
+            Address = address;
+            Type = type;
+            Data = data;
+            DataLength = (byte)(data.Count & 0xFF);
+        }
+
+        /// <summary>
+        /// Formats record to data list, i.e. bytes between ":" and checksum.
+        /// </summary>
+        /// <returns></returns>
+        public List<byte> ToDataList()
+        {
+            var result = new List<byte>();
+
+            result.Add(DataLength); // Length
+            result.Add((byte)((Address & 0xFF00) >> 8)); // High address
+            result.Add((byte)(Address & 0xFF)); // Low address
+            result.Add((byte)(((int)Type) & 0xFF)); // Type
+            result.AddRange(Data);
+
+            return result;
+        }
+    }
+}
---
+++ Client/FloaderClientGUI/LibIntelHexTests/RecordBaseTests.cs
@@ -0,0 +1,52 @@
+﻿using LibIntelHex.Enums;
+using LibIntelHex.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibIntelHexTests
+{
+    /// <summary>
+    /// Tests for base records class
+    /// </summary>
+    [TestFixture]
+    public class RecordBaseTests
+    {
+        /// <summary>
+        /// Testing record conversion into bytes list
+        /// </summary>
+        [TestCaseSource(nameof(TestToDataListConversionSource))]
+        public void TestToDataListConversion(Tuple<int, RecordType, List<byte>, List<byte>> testCase)
+        {
+            var record = new RecordBase(testCase.Item1, testCase.Item2, testCase.Item3);
+            Assert.AreEqual(true, record
+                .ToDataList()
+                .SequenceEqual(testCase.Item4));
+        }
+
+        public static IEnumerable<Tuple<int, RecordType, List<byte>, List<byte>>> TestToDataListConversionSource()
+        {
+            yield return new Tuple<int, RecordType, List<byte>, List<byte>>(
+                0x0, // Address
+                RecordType.EndOfFile, // Type
+                new List<byte>(), // Empty data for End of File
+                new List<byte>() { 0x00, 0x00, 0x00, 0x01 } // Expected result
+                );
+
+            yield return new Tuple<int, RecordType, List<byte>, List<byte>>(
+                0x0040,
+                RecordType.Data,
+                new List<byte>() { 0x21, 0x50, 0x80, 0x40, 0x90, 0x40, 0xE1, 0xF7, 0x00, 0xC0, 0x00, 0x00, 0xEB, 0xCF, 0xF8, 0x94 },
+                new List<byte>() { 0x10, 0x00, 0x40, 0x00, 0x21, 0x50, 0x80, 0x40, 0x90, 0x40, 0xE1, 0xF7, 0x00, 0xC0, 0x00, 0x00, 0xEB, 0xCF, 0xF8, 0x94 }
+                );
+
+            yield return new Tuple<int, RecordType, List<byte>, List<byte>>(
+                0xA55A,
+                RecordType.Data,
+                new List<byte>() { 0x21, 0x50, 0x80, 0x40, 0x90, 0x40, 0xE1, 0xF7, 0x00, 0xC0, 0x00, 0x00, 0xEB, 0xCF, 0xF8, 0x94 },
+                new List<byte>() { 0x10, 0xA5, 0x5A, 0x00, 0x21, 0x50, 0x80, 0x40, 0x90, 0x40, 0xE1, 0xF7, 0x00, 0xC0, 0x00, 0x00, 0xEB, 0xCF, 0xF8, 0x94 }
+                );
+        }
+    }
+}
