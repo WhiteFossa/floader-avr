1873cacb572dd504bb5a5108f8a2a96fccd82dd4
White Fossa <whitefossa@gmail.com>
2020-08-15 09:01:14 +0300

    Data record and tests for it.

---
+++ Client/FloaderClientGUI/LibIntelHex/Models/DataRecord.cs
@@ -0,0 +1,17 @@
+﻿using LibIntelHex.Enums;
+using LibIntelHex.Interfaces;
+using System.Collections.Generic;
+
+namespace LibIntelHex.Models
+{
+    /// <summary>
+    /// Record with data.
+    /// </summary>
+    public class DataRecord : RecordBase
+    {
+        public DataRecord(int address, List<byte> data, IRecordFormatter recordFormatter) : base(address, RecordType.Data, data, recordFormatter)
+        {
+
+        }
+    }
+}
--- Client/FloaderClientGUI/LibIntelHexTests/RecordsTests.cs
+++ Client/FloaderClientGUI/LibIntelHexTests/RecordsTests.cs
@@ -3,6 +3,7 @@ using LibIntelHex.Interfaces;
 using LibIntelHex.Models;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace LibIntelHexTests
 {
@@ -19,11 +20,33 @@ namespace LibIntelHexTests
                 new BytesReaderWriter());
         }
 
+        /// <summary>
+        /// Testing end of file records generation
+        /// </summary>
         [Test]
         public void TestEndOfFileRecordGeneration()
         {
             var record = new EndOfFileRecord(_recordFormatter);
             Assert.AreEqual($":00000001FF{ Environment.NewLine }", record.ToString());
+        }
+
+        /// <summary>
+        /// Testing data records generation
+        /// </summary>
+        [TestCaseSource(nameof(TestDataRecordGenerationSource))]
+        public void TestDataRecordGeneration(Tuple<int, List<byte>, string> testCase)
+        {
+            var record = new DataRecord(testCase.Item1, testCase.Item2, _recordFormatter);
+            Assert.AreEqual(testCase.Item3, record.ToString());
+        }
+
+        public static IEnumerable<Tuple<int, List<byte>, string>> TestDataRecordGenerationSource()
+        {
+            yield return new Tuple<int, List<byte>, string>(
+                0x0040,
+                new List<byte>() { 0x21, 0x50, 0x80, 0x40, 0x90, 0x40, 0xE1, 0xF7, 0x00, 0xC0, 0x00, 0x00, 0xEB, 0xCF, 0xF8, 0x94 },
+                $":10004000215080409040E1F700C00000EBCFF894D1{ Environment.NewLine }"
+                );
         }
     }
 }
