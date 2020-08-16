997127c80cac27aa9f0ba218301fb670f12729a2
White Fossa <whitefossa@gmail.com>
2020-08-16 19:34:14 +0300

    More tests for data blocks.

--- Client/FloaderClientGUI/LibIntelHexTests/DataBlockTests.cs
+++ Client/FloaderClientGUI/LibIntelHexTests/DataBlockTests.cs
@@ -36,5 +36,31 @@ namespace LibIntelHexTests
             Assert.AreEqual(address, block.BaseAddress);
             Assert.AreEqual(data, block.Data.First());
         }
+
+        /// <summary>
+        /// Checks if byte with address 2 can be appended to byte with address 1
+        /// </summary>
+        [TestCase(0, 1, true)]
+        [TestCase(1, 1, false)]
+        [TestCase(1, 0, false)]
+        [TestCase(65534, 65535, true)]
+        public void CheckForIsAppendable(int address1, int address2, bool isAppendable)
+        {
+            var block = new DataBlock(address1, 0x00);
+            Assert.AreEqual(isAppendable, block.IsByteAppendable(address2));
+        }
+
+        /// <summary>
+        /// As CheckForIsAppendable(), but for byte prepending
+        /// </summary>
+        [TestCase(0, 1, false)]
+        [TestCase(1, 1, false)]
+        [TestCase(1, 0, true)]
+        [TestCase(65535, 65534, true)]
+        public void CheckForIsPrependable(int address1, int address2, bool isPrependable)
+        {
+            var block = new DataBlock(address1, 0x00);
+            Assert.AreEqual(isPrependable, block.IsBytePrependable(address2));
+        }
     }
 }
