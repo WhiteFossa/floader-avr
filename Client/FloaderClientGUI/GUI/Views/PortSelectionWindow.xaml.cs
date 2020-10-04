1a8f2c7037ebe8fd45aabfb4a5f5d083b6ca4c75
White Fossa <whitefossa@gmail.com>
2020-10-04 21:45:20 +0300

    Support for double click on port name

--- Client/FloaderClientGUI/GUI/Views/PortSelectionWindow.xaml.cs
+++ Client/FloaderClientGUI/GUI/Views/PortSelectionWindow.xaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using FloaderClientGUI.ViewModels;
 
 namespace FloaderClientGUI.Views
 {
@@ -14,6 +16,20 @@ namespace FloaderClientGUI.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            // Setting up ports list double click handler
+            var portsListBox = this.Find<ListBox>("PortsListBox");
+            portsListBox.DoubleTapped += OnPortDoubleClick;
+        }
+
+        /// <summary>
+        /// Called when user double clicks a port
+        /// </summary>
+        private void OnPortDoubleClick(object? sender, RoutedEventArgs e)
+        {
+            var viewModel = (PortSelectionWindowViewModel)((Control)sender).DataContext;
+
+            viewModel.OK(this);
         }
     }
 }
