5b871a7c896bb59ec73bdc294d85df32f8a0ae45
White Fossa <whitefossa@gmail.com>
2020-11-15 17:03:42 +0300

    Identification must work

--- MCU/Bootloader/main.asm
+++ MCU/Bootloader/main.asm
@@ -1,5 +1,6 @@
 .include "HAL/ATmega16/Defines.inc"
-.include "macros.inc"
+.include "Macros.inc"
+.include "DeviceIdentificationData.inc"
 
 
 .cseg
@@ -43,15 +44,69 @@ EnterBootloader:
 
 							; Command processing loop
 MainLoop:
-		
-							; Just echo (for test)
 							call		UartReadByte
-							call		UartSendByte
 
+							cpi			R16,			'I' ; Identify
+							breq		LblIdentify
+
+							rjmp		MainLoop
+
+; Identification entry point
+LblIdentify:
+							call		Identify
 							rjmp		MainLoop
 
 							; Must never reach this code
 HangForever:
 							rjmp		HangForever
+
+; Call this to identify device
+Identify:
+							push		R16
+							uin			R16,			SREG
+							push		R16
+
+							; Bootloader signature
+							ldi			R16,			'F'
+							call		UartSendByte
+							ldi			R16,			'B'
+							call		UartSendByte
+							ldi			R16,			'L'
+							call		UartSendByte
+
+							; Version (simple bootloader = 0x01)
+							ldi			R16,			0x01
+							call		UartSendByte
+
+							; Vendor identifier
+							ldi			R16,			VendorId2
+							call		UartSendByte
+							ldi			R16,			VendorId1
+							call		UartSendByte
+							ldi			R16,			VendorId0
+							call		UartSendByte
+
+							; Model identifier
+							ldi			R16,			ModelId2
+							call		UartSendByte
+							ldi			R16,			ModelId1
+							call		UartSendByte
+							ldi			R16,			ModelId0
+							call		UartSendByte
+
+							; Serial number
+							ldi			R16,			SerialNumber3
+							call		UartSendByte
+							ldi			R16,			SerialNumber2
+							call		UartSendByte
+							ldi			R16,			SerialNumber1
+							call		UartSendByte
+							ldi			R16,			SerialNumber0
+							call		UartSendByte
+
+							pop			R16
+							uout		SREG,			R16
+							pop			R16
+							ret
 
 .include "HAL/ATmega16/Code.inc"
